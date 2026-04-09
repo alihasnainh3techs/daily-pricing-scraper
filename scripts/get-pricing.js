76263d10677d9a83ec32d0c33a0381406eb45baa
alihasnainh3techs <alimurtaza32721@gmail.com>
2026-04-09 15:15:08 +0500

    pupetter config changes

--- scripts/get-pricing.js
+++ scripts/get-pricing.js
@@ -12,6 +12,7 @@ const __dirname = path.dirname(path.resolve(__filename, ".."));
 (async () => {
     const browser = await puppeteer.launch({
         headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
     const page = await browser.newPage();
